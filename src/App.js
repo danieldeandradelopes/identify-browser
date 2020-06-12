da4def3702568cc7431fe92d787b35432c3ca9a6
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-06-12 11:03:20 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {isFirefox, isChrome, isChromium, isOpera, isSafari, isEdge, isIE, isMobile} from 'react-device-detect';
 
-function App() {
+function App() {  
+  const [browser, setBrowser] = useState('');
+
+  useEffect(() => {
+    if (isFirefox){
+      setBrowser('Firefox')
+    }
+
+    if (isChrome) {
+      setBrowser('Chrome')
+    }
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Você está acessando esse site do navegador: {browser}</h1>
   );
 }
 
